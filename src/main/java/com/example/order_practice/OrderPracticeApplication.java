a8df7a3d159916b5e75dfcad77f96a4b24635026
civilcoy <joungcb06@gmail.com>
2023-03-07 15:57:42 +0900

    Initial commit

---
+++ src/main/java/com/example/order_practice/OrderPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.order_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPracticeApplication.class, args);
+	}
+
+}
